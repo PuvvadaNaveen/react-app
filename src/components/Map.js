1b1998e85e063fb77b61219d548ed1e161d6e6d8
Naveen Kumar Puvvada <Naveen.k.puvvada@centene.com>
2023-02-28 20:58:44 -0600

    performance_upgrade: change dependency from where the map is pulled

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import Map, { Marker } from 'react-map-gl';
-import Pin from './Pin';
+import MapboxGL from 'mapbox-gl';
+
 export default class MapComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             api_url: 'https://data.edmonton.ca/resource/akzz-54k3.json',
+            map: false,
             viewState: {
                 zoom: 14,
-                longitude: -113.4796,
-                latitude: 53.6028
+                center: [ -113.4796, 53.6028 ]
             },
-            coords: [
-                { longitude:-113.4909, latitude:53.5444 },
-                { longitude:-113.6242, latitude:53.5225 },
-                { longitude:-113.4914, latitude:53.5439 }
-            ],
             data: null
         }
     }
 
+    initializeMap() {
+        MapboxGL.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+        let map = new MapboxGL.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            ...this.state.viewState
+        });
+
+        this.setState({ map });
+    }
     componentDidMount() {
         const {data, api_url} = this.state;
 
@@ -32,22 +37,24 @@ export default class MapComponent extends Component {
     }
 
     render () {
-        const { coords, data } = this.state;
+        const { map, data } = this.state;
+        if(data && !map) this.initializeMap()
         return (
-            <Map
-                {...this.state.viewState}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-                mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                style={{width: 700,height: 600}}
-                onMove={(viewState) =>
-                    this.setState({viewState})
-                }>
-                {data && data.map((coord, i) => (
-                    <Marker key={i} longitude={coord.longitude} latitude={coord.latitude} color="red">
-                        <Pin />
-                    </Marker>
-                ))}
-            </Map>
+            // <Map
+            //     {...this.state.viewState}
+            //     mapStyle="mapbox://styles/mapbox/streets-v9"
+            //     mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            //     style={{width: 700,height: 600}}
+            //     onMove={(viewState) =>
+            //         this.setState({viewState})
+            //     }>
+            //     {data && data.map((coord, i) => (
+            //         <Marker key={i} longitude={coord.longitude} latitude={coord.latitude} color="red">
+            //             <Pin />
+            //         </Marker>
+            //     ))}
+            // </Map>
+            <div style={{width:900, height:750}} id="map" />
         )
     }
 }
